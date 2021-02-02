01411c7183f6987b510101362026fde9fe307c92
daeken.kwon <daeken.kwon@gmail.com>
2021-02-02 22:55:52 +0900

    cmake template

---
+++ cmake_template/README.md
@@ -0,0 +1,7 @@
+
+### Goal
+- CMake 를 이용한 C/C++ 빌드 템플릣
+
+
+### Note
+- vscode extention: C/C++, CMake, CMake Tools, cmake-format
---
+++ cmake_template/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+
+  auto name = "it's me, dk";
+  std::cout << "hello world: " << name << std::endl;
+  return 0;
+}
